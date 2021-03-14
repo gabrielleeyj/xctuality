3ea71db2e56d7b70c241f211446ee94ff7a612e7
Gabriel Lee <gabrielleeyj@outlook.com>
2021-03-14 14:02:33 +0800

    generic footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div className="Footer">
+			<h3>Built by Gabriel Lee. Built with ReactJS. Copyright 2021.</h3>
+		</div>
+	);
+}
