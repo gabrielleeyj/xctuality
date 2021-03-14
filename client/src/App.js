449a96d841987b1d153d7db4e50dec8b112343dc
Gabriel Lee <gabrielleeyj@outlook.com>
2021-03-14 14:01:21 +0800

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Cart } from "./components/Cart";
+import { Add } from "./components/Add";
+import { Listing } from "./components/Listing";
+import { Movie } from "./components/Movie";
+import { Footer } from "./components/Footer";
+import { GlobalProvider } from "./context/GlobalState";
+import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
+import "./lib/bootstrap/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Listing} />
+          <Route path="/watchlist" component={Watchlist} />
+          <Route path="/add" component={Add} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/movie/:id" component={Movie} />
+        </Switch>
+        <Footer />
+      </Router>
+    </GlobalProvider>
   );
 }
 
